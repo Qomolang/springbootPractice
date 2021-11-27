a52a098277cc21a2ce659118baccd1ac63c2862f
Qomolang <84028136@qq.com>
2021-11-27 14:02:58 +0800

    可正常打包到target

---
+++ service/src/main/java/ApiController.java
@@ -0,0 +1,11 @@
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+    @RequestMapping("/api")
+    public String index() {
+        return "guten tag";
+    }
+}
--- starter/src/main/java/com/magnus/demo/DemoApplication.java
+++ starter/src/main/java/com/magnus/demo/DemoApplication.java
@@ -3,7 +3,7 @@ package com.magnus.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.magnus.demo"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
