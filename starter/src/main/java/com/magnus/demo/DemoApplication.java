68d68f8442d4db92524f86b7a9fb3eaacb8053a6
Qomolang <84028136@qq.com>
2021-11-27 14:14:01 +0800

    可正常打包到target

--- starter/src/main/java/com/magnus/demo/DemoApplication.java
+++ starter/src/main/java/com/magnus/demo/DemoApplication.java
@@ -3,7 +3,7 @@ package com.magnus.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.magnus.demo"})
+@SpringBootApplication(scanBasePackages = {"com.magnus.demo","com.magnus.service"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
