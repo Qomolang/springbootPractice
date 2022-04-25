b0096417ad3f208062c1854e8551e1988e0f920c
Qomolang <84028136@qq.com>
2022-04-26 01:38:57 +0800

    添加测试模块

---
+++ service/src/test/java/com/magnus/service/infrastruct/utils/ExceptionUtilsTest.java
@@ -0,0 +1,21 @@
+package com.magnus.service.infrastruct.utils;
+
+import com.google.common.collect.Lists;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ExceptionUtilsTest {
+    @Before
+    public void init(){
+
+    }
+
+    @Test
+    public void multiSortTest(){
+        System.out.println("hi");
+    }
+}
